1abe0cc0d1102f14189aefd3e8ef8eb9b6719c7f
Rikardp98 <Rikardp98@gmail.com>
2024-11-07 23:50:29 +0100

    all tomlssss

--- modules/home/yazi/default.nix
+++ modules/home/yazi/default.nix
@@ -4,7 +4,9 @@
     enable = true;
     enableFishIntegration = true;
   };
-  xdg.configFile."yazi/yazi.toml".source = ./yazi.toml;
+  xdg.configFile."yazi/keymap.toml".source = ./keymap.toml;
   xdg.configFile."yazi/theme.toml".source = ./theme.toml;
+  xdg.configFile."yazi/yazi.toml".source = ./yazi.toml;
 }
+
 
