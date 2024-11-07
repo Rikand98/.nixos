ed10105e4c05d0079f62f1f27f7110dffc6788dd
Rikardp98 <Rikardp98@gmail.com>
2024-11-07 18:02:17 +0100

    new

--- modules/core/security.nix
+++ modules/core/security.nix
@@ -2,6 +2,7 @@
 {
   security.rtkit.enable = true;
   security.sudo.enable = true;
+  security.pam.services.gdm-password.enableGnomeKeyring = true;
   # security.pam.services.swaylock = { };
   security.pam.services.hyprlock = {};
 }
