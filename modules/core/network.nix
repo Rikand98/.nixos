b64c3d0ea4cc99b8eae5273a9b85a4d0fa3b50c3
Rikardp98 <Rikardp98@gmail.com>
2024-11-20 16:30:16 +0100

    more ports

--- modules/core/network.nix
+++ modules/core/network.nix
@@ -1,4 +1,4 @@
-{ pkgs, host, ... }: 
+{ pkgs, host, ... }:
 {
   networking = {
     hostName = "${host}";
@@ -6,7 +6,7 @@
     nameservers = [ "8.8.8.8" "8.8.4.4" "1.1.1.1" ];
     firewall = {
       enable = true;
-      allowedTCPPorts = [ 22 80 443 59010 59011 ];
+      allowedTCPPorts = [ 22 80 443 59010 59011 8000 8001 8002 ];
       allowedUDPPorts = [ 59010 59011 ];
     };
   };
