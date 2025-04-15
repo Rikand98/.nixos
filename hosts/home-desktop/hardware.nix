a60f5f2ea714e6cd6b706ea43364ac04ec84193b
rikand <rikardp98@gmail.com>
2025-04-15 21:33:13 +0200

    update hardware

--- hosts/home-desktop/hardware.nix
+++ hosts/home-desktop/hardware.nix
@@ -21,10 +21,6 @@
   "amdgpu.dpm=1"
   "amdgpu.si_support=1"
   "amdgpu.cik_support=1"
-  "radeon.si_support=0"
-  "radeon.cik_support=0"
-  "video=DVI-D-2:1920x1080@144"
-  "video=HDM1-A-1:1920x1080@60"
   ];
   environment.systemPackages = with pkgs; [
     amdgpu_top
