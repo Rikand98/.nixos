13abf2eeaa8e04de9e7898c46e87b267b1d1f9aa
rikand <rikardp98@gmail.com>
2025-04-15 22:00:06 +0200

    ymux

---
+++ modules/home/tmux.nix
@@ -0,0 +1,10 @@
+{ pkgs, ... }:
+{
+   programs.tmux = {
+    enable = true;
+    shell = "${pkgs.fish}/bin/fish";
+    terminal = "tmux-256color";
+    historyLimit = 100000;
+    extraConfig = ''
+  };
+}
