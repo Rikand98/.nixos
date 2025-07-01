b52e91bb22214ede2b15f6ddb2d7a739de76a5d3
Rikardp98 <Rikardp98@gmail.com>
2025-07-01 22:20:22 +0200

    ?

--- modules/home/communication/default.nix
+++ modules/home/communication/default.nix
@@ -2,10 +2,8 @@
 {
   home.packages = with pkgs; [
     teamspeak6-client
-    discord
-    (discord.override {
-     withVencord = true;
-    })
+    discordo
+    webcord-vencord
   ];
   xdg.configFile."Vencord/themes/gruvbox.theme.css".source = ./gruvbox.css;
 }
