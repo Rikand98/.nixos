061bd3b5e3f2712bb2d7c4bcaecc976dbc0bff99
Rikardp98 <Rikardp98@gmail.com>
2025-07-01 22:23:48 +0200

    not ready

--- modules/home/communication/default.nix
+++ modules/home/communication/default.nix
@@ -2,8 +2,8 @@
 {
   home.packages = with pkgs; [
     teamspeak6-client
-    discordo
     webcord-vencord
+    # discordo
   ];
   xdg.configFile."Vencord/themes/gruvbox.theme.css".source = ./gruvbox.css;
 }
