cdeb95ddd05f4ef7e0f9f877b64edf9dd838a9ca
Rikand98 <rikard@rikand.com>
2025-11-08 18:32:13 +0100

    test

--- modules/home/niri/windowrules.nix
+++ modules/home/niri/windowrules.nix
@@ -19,7 +19,7 @@
       open-on-workspace "Chat"
   }
   window-rule {
-      match app-id=r#"^spotify"#
+      match app-id=r#"^spotify|spicetify"#
       open-maximized true
       open-on-output "HDMI-A-1"
       opacity 0.98
